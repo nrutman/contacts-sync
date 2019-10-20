28ed0cddbdc394ca12789925ed958323d9a11bb6
Nate Rutman <nathan.rutman@gmail.com>
2019-10-19 23:46:21 -0400

    IS-1 Add web client factory

---
+++ src/Client/WebClientFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sync\Client;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+
+class WebClientFactory implements WebClientFactoryInterface
+{
+    /** @var array **/
+    protected $defaultConfiguration = [];
+
+    /**
+     * @param array $defaultConfiguration Default configuration for creating Guzzle clients.
+     */
+    public function __construct(array $defaultConfiguration = [])
+    {
+        $this->defaultConfiguration = $defaultConfiguration;
+    }
+
+    /**
+     * Creates a new web client
+     * @param  array           $guzzleConfiguration Configuration for the Guzzle Client
+     * @return ClientInterface                      The instantiated Guzzle Client
+     */
+    public function create(array $guzzleConfiguration = []): ClientInterface
+    {
+        return new Client(array_merge($guzzleConfiguration, $this->defaultConfiguration));
+    }
+}
---
+++ src/Client/WebClientFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sync\Client;
+
+use GuzzleHttp\ClientInterface;
+
+interface WebClientFactoryInterface
+{
+    /**
+     * Creates a new web client given the specified Guzzle configuration SplObjectStorage
+     * @param  array           $guzzleConfiguration Guzzle configuration SplObjectStorage
+     * @return ClientInterface                      The web client that was created
+     */
+    public function create(array $guzzleConfiguration = []): ClientInterface;
+}
