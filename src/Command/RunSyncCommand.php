64b71207341a7054ab7270cac33bc5490a87636f
Nate Rutman <nate.rutman@jazzhr.com>
2019-10-22 22:22:17 -0400

    IS-3 Begin syncing contacts to Google Groups

--- src/Command/RunSyncCommand.php
+++ src/Command/RunSyncCommand.php
@@ -3,10 +3,13 @@
 namespace Sync\Command;
 
 use Carbon\Carbon;
+use Google_Client;
+use Google_Service_Directory_Member;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Sync\Client\GoogleClient;
 use Sync\Client\PlanningCenterClient;
 use Sync\Client\WebClientFactory;
 use Sync\Config\ConfigParser;
@@ -20,12 +23,12 @@ class RunSyncCommand extends Command
     /** @var SymfonyStyle */
     protected $io;
 
-    protected function configure()
+    protected function configure(): void
     {
         $this->setDescription('Syncs contacts from PlanningCenter to Google Groups');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): void
     {
         $this->io = new SymfonyStyle($input, $output);
         $config = ConfigParser::getConfiguration();
@@ -35,22 +38,28 @@ class RunSyncCommand extends Command
             new WebClientFactory()
         );
 
+        $googleClient = new GoogleClient(
+            new Google_Client(),
+            $config['integrations']['google'],
+            dirname(sprintf('%s/../../%s/1', __DIR__, $config['temp']['path']))
+        );
+
         $this->log('Retrieving contacts from Planning Center...');
 
         $contacts = $planningCenterClient->getContacts();
 
         $this->log('Done!');
 
-        $members = [];
+        $church = [];
         $men = [];
         $women = [];
 
-        array_walk($contacts, static function (Contact $contact) use (&$members, &$men, &$women) {
+        array_walk($contacts, static function (Contact $contact) use (&$church, &$men, &$women) {
             if (!in_array($contact->membership, ['Member', 'Regular Attender'])) {
                 return;
             }
 
-            $members[] = $contact;
+            $church[] = $contact;
 
             if ($contact->gender === 'M') {
                 $men[] = $contact;
@@ -63,10 +72,93 @@ class RunSyncCommand extends Command
             'Type',
             'Count',
         ], [
-            ['Members & Regular Attenders', count($members)],
+            ['Members & Regular Attenders', count($church)],
             ['Men', count($men)],
             ['Women', count($women)],
         ]);
+
+        /*
+         * Members & Regular Attenders
+         */
+
+        $this->syncContactsToGoogleGroup(
+            $googleClient,
+            'church@provchurch.org',
+            $church
+        );
+
+        /*
+         * Men's Group
+         */
+
+        $this->syncContactsToGoogleGroup(
+            $googleClient,
+            'men@provchurch.org',
+            $men
+        );
+
+        /*
+         * Women's Group
+         */
+
+        $this->syncContactsToGoogleGroup(
+            $googleClient,
+            'women@provchurch.org',
+            $women
+        );
+    }
+
+    /**
+     * @param GoogleClient $client
+     * @param string $groupId
+     * @param Contact[] $contacts
+     * @param bool $isDryRun
+     */
+    private function syncContactsToGoogleGroup(
+        GoogleClient $client,
+        string $groupId,
+        array $contacts,
+        bool $isDryRun = false
+    ): void {
+        $this->log(sprintf('<info>Sycning %s</info>', $groupId));
+
+        $groupMembers = $client->getGroupMembers($groupId);
+
+        /** @var Contact[] $contactsMappedByEmail */
+        $contactsMappedByEmail = [];
+        foreach ($contacts as $contact) {
+            $contactsMappedByEmail[$contact->email] = $contact;
+        }
+
+        $groupMembersMappedByEmail = [];
+        foreach ($groupMembers as $member) {
+            $groupMembersMappedByEmail[$member->getEmail()] = $member;
+        }
+
+        /** @var Contact[] $toAdd */
+        $toAdd = [];
+        /** @var Google_Service_Directory_Member[] $toRemove */
+        $toRemove = [];
+
+        // Find any emails in the contact list that are not in the Google group. These should be added.
+        array_push($toAdd, ...array_filter($contacts, static function (Contact $contact) use ($groupMembersMappedByEmail) {
+            return !isset($groupMembersMappedByEmail[$contact->email]);
+        }));
+
+        // Find any Google group members that are not in the contact list. These should be removed.
+        array_push($toRemove, ...array_filter($groupMembers, static function (Google_Service_Directory_Member $member) use ($contactsMappedByEmail) {
+            return !isset($contactsMappedByEmail[$member->getEmail()]);
+        }));
+
+        $this->io->text(sprintf('Adding %d contacts...', count($toAdd)));
+        $this->io->listing(array_map(static function (Contact $contact) {
+            return sprintf('%s %s (%s)', $contact->firstName, $contact->lastName, $contact->email);
+        }, $toAdd));
+
+        $this->io->text(sprintf('Removing %d contacts...', count($toRemove)));
+        $this->io->listing(array_map(static function (Google_Service_Directory_Member $member) {
+            return $member->getEmail();
+        }, $toRemove));
     }
 
     private function log(string $message): void
