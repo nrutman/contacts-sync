c1edfe8a6a0da5774bf56bc6a6dc58ee93d7c798
Nate Rutman <nathan.rutman@gmail.com>
2019-12-07 21:56:05 -0500

    IS-25 Rename diff to analyzer and add de-duping

--- tests/Contact/ContactListAnalyzerTest.php
+++ tests/Contact/ContactListAnalyzerTest.php
@@ -3,12 +3,12 @@
 namespace App\Test\Contact;
 
 use App\Contact\Contact;
-use App\Contact\ContactListDiff;
+use App\Contact\ContactListAnalyzer;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ContactListDiffTest extends MockeryTestCase
+class ContactListAnalyzerTest extends MockeryTestCase
 {
-    public function test_diff()
+    public function test_diff(): void
     {
         $sharedContact = new Contact();
         $sharedContact->email = 'foo@bar';
@@ -17,11 +17,23 @@ class ContactListDiffTest extends MockeryTestCase
         $missingContact = new Contact();
         $missingContact->email = 'shaz@shuz';
 
-        $target = new ContactListDiff([$sharedContact, $missingContact], [$sharedContact, $extraContact]);
+        $target = new ContactListAnalyzer([$sharedContact, $missingContact], [$sharedContact, $extraContact]);
 
         $this->assertCount(1, $target->getContactsToAdd());
         $this->assertEquals($missingContact, $target->getContactsToAdd()[0]);
         $this->assertCount(1, $target->getContactsToRemove());
         $this->assertEquals($extraContact, $target->getContactsToRemove()[0]);
+    }
+
+    public function test_duplicates(): void
+    {
+        $contact = new Contact();
+        $contact->email = 'foo@bar';
+        $duplicate = new Contact();
+        $duplicate->email = 'foo@bar';
+
+        $target = new ContactListAnalyzer([$contact, $duplicate], []);
+
+        $this->assertCount(1, $target->getContactsToAdd());
     }
 }
